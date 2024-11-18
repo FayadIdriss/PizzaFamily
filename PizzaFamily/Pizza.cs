72bb84c2e6a98a0d59ed89fbf955d5b73fdfa75a
FayadIdriss <fayadbacari-dev@outlook.com>
2024-11-18 18:43:15 +0100

    added exercise 1

---
+++ PizzaFamily/Pizza.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace PizzaFamily
+{
+	public class Pizza
+	{
+		public string nom;
+		public int prix;
+		public bool vegetarien;
+
+		public Pizza (string Nom, int Prix, bool Vegetarien)
+		{
+			this.nom = Nom;
+			this.prix = Prix;
+			this.vegetarien = Vegetarien;
+		}
+
+
+		public void Afficher()
+		{
+            if(vegetarien == true)
+			{
+				Console.WriteLine("Pizza " + nom + "  " + prix + "" + "euro" + " " + "(V)");
+			}
+			else
+			{
+                Console.WriteLine("Pizza " + nom + "  " + prix + "" + "euro");
+            }
+        }
+	}
+}
+
--- PizzaFamily/Program.cs
+++ PizzaFamily/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace PizzaFamily
+{
+    /// <summary>
+    /// Ceci est une implémentation du message traditionnel « Hello world! »
+    /// </summary>
+    public class PizzaFamily
+    {
+        /// <summary>
+        /// Le programme commence ici
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            Pizza myPizza1 = new Pizza("Boisé", 12, false);
+            myPizza1.Afficher();
+
+            Pizza myPizza2 = new Pizza("Végétarien", 10, true);
+            myPizza2.Afficher();
+        }
+    }
+}
